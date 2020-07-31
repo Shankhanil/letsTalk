ede42aa939f68f29da6c0f32b1d227e1759e4001
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-07-31 23:14:05 +0530

    milestone-2

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ io.on('connection', (socket) =>{
     socket.on('disconnect', ()=>{
         console.log('ohh no, user has left!!!');
     });
+	socket.on('join', ({name, room}, callback)=>{
+		console.log(name, room);
+		
+	});
 });
 
 app.use(router);
