019421acadfc6476a995737ae7089164dc446584
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-07-31 21:48:55 +0530

    chat update

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,13 +2,25 @@ import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+
+let socket;
 const Chat = ({ location }) => {
+	const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+	const ENDPOINT = 'localhost:5000';
 	useEffect( ()=>{
 		
-		const data = queryString.parse(location.search);
-		console.log(location.search);
-		console.log(`${data.name}, ${data.room}`);
-	} )
+		const {name, room}= queryString.parse(location.search);
+		// console.log(location.search);
+		// console.log(name, room);
+		
+		socket = io(ENDPOINT);
+		
+		setName(name);
+		setRoom(room);
+		
+		console.log(socket)
+	}, [ENDPOINT, location.search] )
     return(
         <h1> Chat </h1>
     )
