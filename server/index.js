3b3d37e4093aa1f360becadfc3d4d9d1e9dc461e
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-08-01 11:39:32 +0530

    message sending funtionality complete

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const {addUser, removeUser, getUser, getUserInRoom } = require('./users');
+
 io.on('connection', (socket) =>{
     console.log('we have a new connection!!!!');
     
@@ -20,7 +22,26 @@ io.on('connection', (socket) =>{
 	socket.on('join', ({name, room}, callback)=>{
 		console.log(name, room);
 		
+		const {error, user} = addUser({ id:socket.id, name, room });
+		
+		if(error) return callback(error);
+		
+		socket.emit('message', {user : 'admin', text: `${user.name}, welcome to ${user.room}`} );
+		
+		socket.broadcast.to(user.room).emit('message', {user : 'admin', text: `${user.name} joined the room`});
+		
+		socket.join(user.room);
+		
+		callback();
 	});
+	socket.on('sendMessage', ({message}, callback)=>{
+		const user = getUser(socket.id);
+		
+		io.to(user.room).emit('message', {user: user.name, text: message} );
+		
+		callback();
+	});
+	
 });
 
 app.use(router);
