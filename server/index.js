1ca91c73ac36e0441b4b53b25fcf7e1ffa8e1fd1
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-08-04 17:01:53 +0530

    fix bugs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -52,4 +53,5 @@ io.on('connection', (socket) =>{
 });
 
 app.use(router);
+app.use(cors());
 server.listen(PORT, ()=> console.log(`server is running at ${PORT}`));
